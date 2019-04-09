eb351c6d35ab70d544de94f8e60ca52dfd19ec29
Adarshreddy adelli <adarshreddy9849@gmail.com>
2019-04-09 18:11:47 +0530

    Update settings.py

--- my_project/settings.py
+++ my_project/settings.py
@@ -10,11 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
-import dj-database-url # add this to the top of your file
-
-db_from_env = dj_database_url.config()
- DATABASES[‘default’].update(db_from_env)
-
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -97,7 +92,10 @@ DATABASES = {
     }
 }
 
+import dj-database-url # add this to the top of your file
 
+db_from_env = dj_database_url.config()
+DATABASES[‘default’].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
